b40411333804bce6368bed219b5e1719fbf2b7c3
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-02-27 11:31:51 +0200

    modify app.js

--- src/app.js
+++ src/app.js
@@ -13,16 +13,16 @@ app.use(favicon(path.join(__dirname, '..', 'public', 'library.ico')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
-    extended: false,
+  extended: false,
 }));
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', exphbs({
-    extname: 'hbs',
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'views', 'partials'),
-    defaultLayout: 'main'
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
+  defaultLayout: 'main',
 }));
 app.use(controllers);
 
