38744dc4627a80ad1b6c02fd46bd050341d477b6
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-02-27 11:39:28 +0200

    modify the controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,16 @@
 const express= require('express');
+
 const router = express.Router();
 const home = require('./home');
 const loginPage = require('./loginPage');
 const checkUser = require('./checkUser');
+const signUpPage = require('./signUpPage');
+const adduser = require('./adduser');
 
-router.get('/',home.get);
+router.get('/', home.get);
 router.get('/login', loginPage.display);
 router.post('/check', checkUser.checkUser);
+router.get('/signup', signUpPage.sign);
+router.post('/adduser', adduser.add);
 
 module.exports = router;
